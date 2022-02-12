2055a25828988af7f258e69d287a921af770bffe
IceTeaXXD <16521516@mahasiswa.itb.ac.id>
2022-02-12 18:48:39 +0700

    Updated Misi 2 Programming

--- 16521516_Ahmad Nadil/Misi_2_Programming-SEKURO_2022-16521516_Ahmad_Nadil.c
+++ 16521516_Ahmad Nadil/Misi_2_Programming-SEKURO_2022-16521516_Ahmad_Nadil.c
@@ -4,58 +4,77 @@
 float fPlus(float a, float b) {
     /* Function tambah */
     /* Mengembalikan value a + b */
+    return a+b;
 }
 
 void pPlus(float *a, float b) {
     /* Procedure tambah */
     /* I.S. a dan b terdefinisi dan tidak sembarang */
     /* F.S. a menjadi a + b */
+    *a = *a + b;
 }
 
 float fMin(float a, float b) {
     /* Function kurang */
-    /* Mengembalikan value a + b */
+    /* Mengembalikan value a - b */
+    return a-b;
 }
 
 void pMin(float *a, float b) {
     /* Procedure kurang */
     /* I.S. a dan b terdefinisi dan tidak sembarang */
     /* F.S. a menjadi a - b */
+    *a = *a - b;
 }
 
 float fTimes(float a, float b) {
     /* Function kali */
     /* Mengembalikan value a * b */
+    return a*b;
 }
 
 void pTimes(float *a, float b) {
     /* Procedure kali */
     /* I.S. a dan b terdefinisi dan tidak sembarang */
     /* F.S. a menjadi a * b */
+    *a = *a * b;
 }
 
 float fDivide(float a, float b) {
     /* Function bagi */
     /* Mengembalikan value a / b */
+    return a/b;
 }
 
 void pDivide(float *a, float b) {
     /* Procedure bagi */
     /* I.S. a dan b terdefinisi dan tidak sembarang */
     /* F.S. a menjadi a * b */
+    *a = *a / b;
 }
 
 float fPower(float a, float b) {
     /* Function pangkat */
     /* Asumsi b adalah int */
     /* Mengembalikan value a ^ b */
+    float hasil = 1;
+    int i;
+    for (i = 0; i < b; i++) {
+        hasil = hasil * a;
+    }
+    return hasil;
 }
-
 void pPower(float *a, float b) {
     /* Procedure pangkat */
     /* Asumsi b adalah int */
     /* I.S. a dan b terdefinisi dan tidak sembarang */
     /* F.S. a menjadi a ^ b */
+    float hasil = 1;
+    int i;
+    for (i = 0; i < b; i++) {
+        hasil = hasil * *a;
+    }
+    *a = hasil;
 }
 
 int main() {
@@ -64,5 +83,7 @@ int main() {
               prosedur yang telah didefinisikan dan diimplementasi sebelumnya */
     /* Input dan output dibebaskan kepada Cakru URO 14 */
     /* Batasan: Derajat dari koefisien adalah whole number {0, 1, 2, ...} */
+    float a = 2;
+    float b = 3;
     return 0;
 }

95df59205f0ff7bf8ed25335a57b17e2eb2b3ece
adrsmr <100467225+adrsmr@users.noreply.github.com>
2022-02-27 22:42:01 +0700

    16521391_Adriel Simamora
    Code Fix

--- Misi 2 Programming - SEKURO 2022.c
+++ Misi 2 Programming - SEKURO 2022.c
@@ -4,71 +4,68 @@
 float fPlus(float a, float b) {
     /* Function tambah */
     /* Mengembalikan value a + b */
-    return a+b;
-}
-
-void main(){
-    printf("Hasil 4.5 + 3.5: %.2f\n", fPlus(4.5,3.5));
+    return(a+b);
 }
 
 void pPlus(float *a, float b) {
     /* Procedure tambah */
     /* I.S. a dan b terdefinisi dan tidak sembarang */
     /* F.S. a menjadi a + b */
+    *a +=b;
 }
 
 float fMin(float a, float b) {
-    /* Function tambah */
+    /* Function kurang */
     /* Mengembalikan value a + b */
-    return a-b;
-}
-
-void main(){
-    printf("Hasil 4.5 - 3.5: %.2f\n", fMin(4.5,3.5));
+    return (a-b);
 }
 
 void pMin(float *a, float b) {
     /* Procedure kurang */
     /* I.S. a dan b terdefinisi dan tidak sembarang */
     /* F.S. a menjadi a - b */
+    *a -=b;
 }
 
 float fTimes(float a, float b) {
-    /* Function tambah */
-    /* Mengembalikan value a + b */
-    return a*b;
-}
-
-void main(){
-    printf("Hasil 4.5 + 3.5: %.2f\n", fTimes(4.5,3.5));
+    /* Function kali */
+    /* Mengembalikan value a * b */
+    return (a*b);
 }
 
 void pTimes(float *a, float b) {
     /* Procedure kali */
     /* I.S. a dan b terdefinisi dan tidak sembarang */
     /* F.S. a menjadi a * b */
+    *a *= b;
 }
 
 float fDivide(float a, float b) {
-    /* Function tambah */
-    /* Mengembalikan value a + b */
-    return a/b;
-}
-
-void main(){
-    printf("Hasil 4.5 + 3.5: %.2f\n", fDivide(4.5,3.5));
+    /* Function bagi */
+    /* Mengembalikan value a / b */
+    return(a/b);
 }
 
 void pDivide(float *a, float b) {
     /* Procedure bagi */
     /* I.S. a dan b terdefinisi dan tidak sembarang */
     /* F.S. a menjadi a * b */
+    *a /= b;
 }
 
 float fPower(float a, float b) {
     /* Function pangkat */
     /* Asumsi b adalah int */
     /* Mengembalikan value a ^ b */
+    if b == 0{
+        return 1;
+    }
+    else if b>0{
+        return(fTimes(a,fPower(a,b-1)))
+    }
+    else { // Pangkat negatif
+        return(1/(fTimes(a,fPower(a,b-1)))
+    }
 }
 
 void pPower(float *a, float b) {
@@ -76,6 +73,12 @@ void pPower(float *a, float b) {
     /* Asumsi b adalah int */
     /* I.S. a dan b terdefinisi dan tidak sembarang */
     /* F.S. a menjadi a ^ b */
+    float hasil = 1;
+    int i;
+    for (i = 0; i < b; i++) {
+        hasil = hasil * *a;
+    }
+    *a = hasil;
 }
 
 int main() {
@@ -84,5 +87,15 @@ int main() {
               prosedur yang telah didefinisikan dan diimplementasi sebelumnya */
     /* Input dan output dibebaskan kepada Cakru URO 14 */
     /* Batasan: Derajat dari koefisien adalah whole number {0, 1, 2, ...} */
+    float a, b, n, h, sum;
+    printf("Enter a: "); scanf("%f", &a);
+    printf("Enter b: ");scanf("%f", &b);
+    printf("Enter n: ");scanf("%f", &n);
+    h = (b - a) / n;
+    sum = 0;
+    for (int i = 1; i <= n; i++) {
+        sum += fPlus(a, i * h);
+    }
+    printf("The result is: %f\n", sum * h);
     return 0;
 }
